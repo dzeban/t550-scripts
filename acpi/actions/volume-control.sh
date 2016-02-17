eafe307b2e026925d2c0f9c8d161e251bca29383
Alex Dzyoba <alex@dzyoba.com>
2016-02-17 16:50:37 +0300

    VOL: Remove excessive logging from acpi action

--- acpi/actions/volume-control.sh
+++ acpi/actions/volume-control.sh
@@ -4,9 +4,6 @@ LOGGER="logger -t $0"
 SINK=$(pactl info | awk -F ':' '$1 == "Default Sink" {print $2}')
 PAINFO=$(pactl info 2>&1)
 
-$LOGGER "Sink is '$SINK'"
-$LOGGER "PAINFO is '$PAINFO'"
-
 case $1 in
 	*MUTE*)
 		$LOGGER -t $0 "Muting $SINK sink"
