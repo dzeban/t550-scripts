9b132f1216b2df75783d8255ae28e279eab5183b
Alex Dzyoba <alex@dzyoba.com>
2016-02-18 12:28:23 +0300

    MON: add udev rule and script for monitor-hotplug
    Monitor hotplug detected by udev when "drm" subsystem changes. Note, that this
    event will be received only when your graphic driver supports DRM, though almost
    every not horribly old driver does so.
    
    When udev receives drm change event, it invokes
    /etc/udev/scripts/monitor-hotplug.sh that applies configuration found in
    /etc/layout/<number of monitors>.sh, so for a single monitor it will invoke
    /etc/layout/1.sh, for 2 it will be /etc/layout/2.sh and so on.
    
    Configuration is xrandr script, that can be generated with `arandr` utility or
    anything same.

--- udev/scripts/monitor-hotplug.sh
+++ udev/scripts/monitor-hotplug.sh
@@ -1,5 +1,59 @@
 #!/bin/bash -eu
 
-path="/sys$1"
-echo "Activated $path" >> /tmp/monitor-hotplug.log
+LOGGER="logger -t $0"
+N_MONITORS=0
 
+function get_monitors_count()
+{
+    echo $(xrandr -q | grep " connected" | wc -l )
+}
+
+# Apply configuration depending on monitors count
+function apply_configuration()
+{
+    local n=$1
+
+    if [[ ! -x /etc/layout/$n.sh ]]; then
+        $LOGGER "No configuration for $n monitors, create it with `arandr` and save it to /etc/layout"
+        exit 1
+    fi
+
+    $LOGGER "Applying configuration for $n monitors"
+    /etc/layout/$n.sh
+}
+
+# Determine configuration changes, exactly monitor count changes.
+# Uses and updates N_MONITORS
+# Return 0 if not changed, 1 if changed
+function changed_configuration()
+{
+    local n=$(get_monitors_count)
+    if [[ "$n" -ne "$N_MONITORS" ]]; then
+        N_MONITORS="$n"
+        return 0
+    else
+        return 1
+    fi
+}
+
+export DISPLAY=:0
+
+# Upon receiving event from kernel we apply configuration and 
+# keep polling for the new state for 10 more seconds.
+# We have to do this, because monitors don't appear immediately in xrandr
+# on cable plug/unplug, e.g. DisplayPort devices may take more than 
+# 5 seconds to appear. So xrandr reports old configuration.
+N_MONITORS=$(get_monitors_count)
+apply_configuration $N_MONITORS
+
+seconds=20 # Sigh...
+while ((seconds > 0)); do
+    if changed_configuration; then
+        apply_configuration $N_MONITORS
+		break
+    fi
+    sleep 1
+    seconds=$((seconds-1))
+done
+
+# vim: ts=4: sw=4: expandtab
